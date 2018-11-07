d7c9817e557b26e6e6f22d430aeab51c1cf584ef
Jiayi Lin <rlin1@brynmawr.edu>
2018-11-06 19:19:13 -0500

    clarification needed, and reflection needed

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -6,24 +6,23 @@ from nltk.tag import hmm
 brown_news_tagged = brown.tagged_words(categories='news', tagset='universal')
 tag_fd = nltk.FreqDist(tag for (word, tag) in brown_news_tagged)
 most_common = tag_fd.most_common()[:8]
+print("8 most frequent tags are: {}".format(most_common))
 
+
+# What are the 15 most frequent nouns?
+# What are the 15 most frequent verbs?
+# What are the 15 most frequent adjectives?
+# What are the 15 most frequent adverbs?=
 def find_most_frequent(freqdist, tagname, count):
     result = []
-    for word, tag in brown_news_tagged:
+    for (word, tag) in brown_news_tagged:
         if tag == tagname:
-            if word+"/"+tag in ntable.keys():
+            if tag == tagname:   
                 freqdist[word+"/"+tag] += 1
-            else:
-                freqdist[word+"/"+tag] = 1
     for (word_tag, num) in freqdist.most_common()[:count]:
         result.append((num, word_tag))
     return result
 
-# print(brown_news_tagged[0:20])
-# What are the 15 most frequent nouns?
-# What are the 15 most frequent verbs?
-# What are the 15 most frequent adjectives?
-# What are the 15 most frequent adverbs?
 ntable = nltk.FreqDist()
 vtable = nltk.FreqDist()
 adjtable = nltk.FreqDist()
@@ -33,50 +32,88 @@ freq_noun = find_most_frequent(ntable, 'NOUN', 15)
 freq_verb = find_most_frequent(vtable, 'VERB', 15)
 freq_adj = find_most_frequent(adjtable, 'ADJ', 15)
 freq_adv = find_most_frequent(advtable, 'ADV', 15)
-print("15 most frequent nouns: {}\n".format(freq_noun))
-print("15 most frequent verbs: {}\n".format(freq_verb))
-print("15 most frequent adj: {}\n".format(freq_adj))
-print("15 most frequent adv: {}\n".format(freq_adv))
+print("15 most frequent nouns:")
+for pair in freq_noun:
+    print('{} appears {} times'.format(pair[1], pair[0]))
+print("----------------------------------")
+print("15 most frequent verbs: ")
+for vpair in freq_verb:
+    print('{} appears {} times'.format(vpair[1],vpair[0]))
+print("----------------------------------")
+print("15 most frequent adj: ")
+for pair in freq_adj:
+    print('{} appears {} times'.format(pair[1],pair[0]))
+print("----------------------------------")
+print("15 most frequent adv: ")
+for pair in freq_adv:
+    print('{} appears {} times'.format(pair[1],pair[0]))
 print("----------------------------------")
 
-def tagging_system(text):
+# Tagging system
+def tagging_system(text, name):
     bts = brown.tagged_sents(categories="news", tagset="universal")
+    # train hmmtagr on all bts and use hmmtagger to evaluate the result of unigramTagger
     hmmTagr = hmm.HiddenMarkovModelTagger.train(bts)
+    uTagr = nltk.UnigramTagger(bts)
     tsent = nltk.word_tokenize(text)
-    tagged_sent = hmmTagr.tag(tsent)
-    print(hmmTagr.evaluate(bts))
-    print(tagged_sent)
+    tagged_sent = [uTagr.tag(tsent)]
+    # print out the accuracy and the tagged text
+    print("the accuracy of {} is :{}".format(name,hmmTagr.evaluate(tagged_sent)))
+    print("------Below is the outcome of UnigramTagging of text: {}------".format(name))
+    print(tagged_sent[0])
 
 with open("./text1.txt") as f:
-    data = f.read()
-tagging_system(data)
+    data1 = f.read()
+with open("./text2.txt") as f:
+    data2 = f.read()
+with open("./my_test.txt") as f:
+    my_data1 = f.read()
+with open('./my_test1.txt') as f:
+    my_data2 = f.read()
+tagging_system(data1, 'text1.txt')
+tagging_system(data2, 'text2.txt')
+tagging_system(my_data1, 'my_test.txt')
+tagging_system(my_data2, 'my_test1.txt')
 
 # extra credit
 patterns = [
     (r'[\.,:;?!]$', '.'), # punctuation
     (r'^-?[0-9]+(.[0-9]+)?$', 'NUM'), # cardinal numbers
     (r'(The|the|A|a|An|an|no|No)$', 'DET'), # articles
-    (r'(some|Some|most|Most|every|Every|which|Which)$','DET'),
-    (r'(At|at|On|on|Out|out|Over|over|Per|per|That|that|Up|up|Down|down|Into|into|Under|under|By|by|With|with)$', 'PRT'), # articles
+    (r'(some|Some|most|Most|every|Every|which|Which|who|Who)$','DET'),
+    (r'(onto|Onto|At|at|On|on|Out|out|Over|over|Up|up|Down|down|Into|into|Under|under|By|by|With|with)$', 'PRT'), # articles
+    (r'(Per|per|That|that)$', 'PRT'),
+    (r'(of|Of|from|From)$', 'ADP'),
     (r'.*able$', 'ADJ'), # adjectives
-    (r'(now|Now)$', 'ADJ'),
+    (r'(now|Now|still|Still|biped)$', 'ADJ'),
+    (r'(Polly)$', 'NOUN'),
     (r'.*ous$', 'ADJ'), 
-    (r'.*ly$', 'ADV'), # adverbs
     (r'(and|And|or|Or|but|But|if|If|while|While|although|Although)$', 'CONJ'),
     (r'(many|Many|much|Much|such|Such)$', 'ADV'),
-    (r'.*ing$', 'VERB'), # gerund
-    (r'.*ed$', 'VERB'), # simple past
+    (r'(engineering)$','NOUN'),
+    (r'(is|was|were|are|Is|Was|Were|Are|have|Have|has|Has|had|Had)$', 'VERB'),
+    (r'(play|Play|transcend|swims|bite|fly|walk|walks)$','VERB'), 
+    (r'(Socrates|Collies|Beagles|Canaries)$', 'NOUN'),
+    (r'.*ly$', 'ADV'), # adverbs
     (r'.*es$', 'VERB'), # 3rd person singular present
     (r'.*ould$', 'VERB'), # modal
+    (r'.*ing$', 'VERB'), # gerund
+    (r'.*ed$', 'VERB'), # simple past
     (r'.*\'s$', 'NOUN'), # possessive noun
-    (r'.*$', 'NOUN'), # noun default
-    (r'(He|he|She|she|It|it|I|me|Me|You|you)$', 'PRP'), # pronouns
+    (r'(He|he|His|his|She|she|Her|her|It|it|its|Its|I|me|Me|my|My|You|you|your|Your|Our|our|us|Us|We|we|they|They|their|Their|them|Them)$', 'PRON'), # pronouns
+    (r',*ary$', 'ADJ'),
     (r'.*s$', 'NOUN'), # plural noun
     (r'.*ness$', 'NOUN'), # nouns from adjectives
+    (r'.*$', 'NOUN'), # noun default
 ]
 reTagr = nltk.RegexpTagger(patterns)
 test_sent = brown.sents(categories='news')[0]
-tsent = nltk.word_tokenize(data)
-print('-------')
+tsent = nltk.word_tokenize(data1)
+print('---------Extra credit--------')
 tag_sent = reTagr.tag(tsent)
+print("------Below is the outcome of regular expression tagger of text: {}------".format('text1.txt'))
+print(tag_sent)
+tsent = nltk.word_tokenize(data2)
+tag_sent = reTagr.tag(tsent)
+print("------Below is the outcome of regular expression tagger of text: {}------".format('text2.txt'))
 print(tag_sent)
---
+++ hw4/reflection.md
@@ -0,0 +1,9 @@
+Assignment 4
+
+Rose Lin
+
+* The exercise helps me get more familiar with the tagging methods of nltk, and the two tutorials are really helpful.
+
+* The tagging system is designed as follows:
+
+** 
